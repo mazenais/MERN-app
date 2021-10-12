2a9597b0b4d552244289fce0ba81c4b4367b3b86
mazenais <mazen.aboismail@gmail.com>
2021-10-12 17:18:13 +0200

    fixing error with passport

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const Login = () => {
+    const [state, setState] = useState
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Login
